ee080ef0c7e9e571b755716a0e4e5588c5b382b0
RyosukeKamimura <come1234560@gmail.com>
2020-08-08 17:57:28 +0900

    https://qiita.com/orimomo/items/3120aff12a06f0ab14e2🧑🏻‍💻

--- Study_RxSwift/ViewController.swift
+++ Study_RxSwift/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textfield: UITextField!
+    
+    
+    //
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textfield.rx.text.orEmpty.bind(to: label.rx.text).disposed(by: disposeBag)
+        
+        //
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
 
